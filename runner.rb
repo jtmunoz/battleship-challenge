f0248589e3526b5e6c049347d4523a5585d7c71e
Tae Yun <email": null>
2016-10-30 00:05:10 -0700

    start updating

--- runner.rb
+++ runner.rb
@@ -1,1 +1,10 @@
 require_relative 'battleship'
+
+  #name, size, quantity
+  ship_hash ={
+              "Carrier" => [5, 1],
+              "Battleship" => [4, 1],
+              "Cruiser" => [3, 1],
+              "Destroyer" => [2, 2],
+              "Submarine" => [1, 2],
+              }
