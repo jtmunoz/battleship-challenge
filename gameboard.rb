620c3d172ce05ac9ab6aff9e8b0649672aa7066a
Dimesky <matthew.dimesky@gmail.com>
2016-11-05 23:44:50 -0700

    implement random vertical placement method

--- gameboard.rb
+++ gameboard.rb
@@ -26,11 +26,13 @@ class Gameboard
 		end
 	end
 
-	def add_hidden_spots(pos)
-		if pos.to_s.length > 1
-			self.hidden_board[pos.to_s[0].to_i][pos.to_s[1].to_i] = "X"
-		else
-			self.hidden_board[0][pos] = "X"
+	def show_hidden_spots
+		@taken_positions.flatten!.each do |position|
+			if position.to_s.length > 1
+				self.hidden_board[position.to_s[0].to_i][position.to_s[1].to_i] = "X"
+			else
+				self.hidden_board[0][position] = "X"
+			end
 		end
 	end
 
@@ -72,13 +74,46 @@ class Gameboard
 			end
 		end
 		p "spots array after do #{spots_array}"
-		# @taken_positions << spots_array
+		ship.position = spots_array
 		p @taken_positions
 		return spots_array
 	end
 
 	def continue_placing_ship_vertically(ship)
-
+		initial_spot = test_for_taken_spots(rand(99))
+		p "HI"
+		spots_array = Array.new << initial_spot
+		p "spots_array before while statement: #{spots_array}"
+		forwards_increment = 10
+		backwards_increment = 10
+		while spots_array.length < ship.size
+			if !((initial_spot + forwards_increment) > 99)
+				next_spot = initial_spot + forwards_increment
+				spots_array << next_spot
+				forwards_increment += 10
+			else 
+				next_spot = initial_spot - backwards_increment
+				spots_array << next_spot
+				backwards_increment += 10
+			end
+		end
+		p "spots_array after while statement: #{spots_array}"
+		@taken_positions.flatten! #flattening array so check can occur with numbers instead of other arrays
+		@taken_positions << spots_array
+		p "taken_positions #{@taken_positions.flatten} after flattening and spots_array #{spots_array} before check loop"
+		spots_array.each do |spot|
+			if @taken_positions.include?(spot)
+				p true
+				@taken_positions.pop
+				continue_placing_ship_vertically(ship)
+				p spots_array
+			end
+		end
+		p "spots array after do #{spots_array}"
+		ship.position = spots_array
+		# @taken_positions << spots_array
+		p @taken_positions
+		return spots_array
 	end
 
 	def position_ships
@@ -88,17 +123,16 @@ class Gameboard
 		continue_placing_ship_horizontally(fleet.fleet_ships[:destroyer2])
 		continue_placing_ship_horizontally(fleet.fleet_ships[:cruiser])
 		continue_placing_ship_horizontally(fleet.fleet_ships[:battleship])
-		continue_placing_ship_horizontally(fleet.fleet_ships[:carrier])
+		continue_placing_ship_vertically(fleet.fleet_ships[:carrier])
 	end
 
 	def show_hidden_board
+		show_hidden_spots
 		puts "   A  B  C  D  E  F  G  H  I  J"
 		@hidden_board.each_with_index do |row, index|
 			puts "#{index}  #{row.join('  ')}"
 		end
 	end
-
-
 
 end
 
--- runner.rb
+++ runner.rb
@@ -8,35 +8,6 @@ require_relative 'fleet'
 require_relative 'gameboard'
 
 
-# carrier = Carrier.new
-
-
-# p carrier.class
-# p carrier.size
-
-# p carrier.is_sunk?
-# p carrier.times_hit
-# carrier.hit 
-# 4.times {carrier.hit}
-# p carrier.times_hit
-# p carrier.is_sunk?
-
-# p fleet
-# p fleet.is_fleet_sunk?
-# p fleet.fleet_hash[:carrier].is_sunk?
-# 5.times {fleet.fleet_hash[:carrier].hit}
-# p fleet.fleet_hash[:carrier].is_sunk?
-# 4.times {fleet.fleet_hash[:battleship].hit}
-# p fleet.fleet_hash[:battleship].is_sunk?
-# p fleet.is_fleet_sunk?
-# 3.times {fleet.fleet_hash[:cruiser].hit}
-# p fleet.fleet_hash[:cruiser].is_sunk?
-# 2.times {fleet.fleet_hash[:destroyer1].hit}
-# 2.times {fleet.fleet_hash[:destroyer2].hit}
-# 1.times {fleet.fleet_hash[:submarine1].hit}
-# p fleet.is_fleet_sunk?
-# 1.times {fleet.fleet_hash[:submarine2].hit}
-# p fleet.is_fleet_sunk?
 fleet = Fleet.new
 gameboard = Gameboard.new(fleet)
 
@@ -49,7 +20,13 @@ gameboard.show_hidden_board
 
 p gameboard.taken_positions
 
-
+p fleet.fleet_ships[:submarine1].position
+p fleet.fleet_ships[:submarine2].position
+p fleet.fleet_ships[:destroyer1].position
+p fleet.fleet_ships[:destroyer2].position
+p fleet.fleet_ships[:cruiser].position
+p fleet.fleet_ships[:battleship].position
+p fleet.fleet_ships[:carrier].position
 
 
 
