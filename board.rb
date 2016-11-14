344186a78b6dca12fee4eb5e22c46e9325554bd9
Jordan Munoz <email": null>
2016-11-13 20:38:34 -0800

    finalizes board

--- board.rb
+++ board.rb
@@ -1,7 +1,6 @@
 
 
 
-# Your code here
 
 #creates a board
 def board_size
@@ -14,33 +13,39 @@ def board_columns
   columns =
   ["  ","A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
   board = board_size
-  board[0].replace(columns)
+  board[0] = (columns)
   #board_size[0].map! do |cell|
-  #  cell = "A"
+  board
 end
 
 def board_rows
   i = 0
-  board_size.each do |row|
+  board_columns.each do |row|
     row[0] = integer(i)
     i += 1
   end
 end
 
 def board
-  board_rows << board_columns
+  board_rows
 end
 
 def integer(number)
-  if number == 10
-    number.to_s
-  else
-    number.to_s + " "
+  case number
+    when 0
+      "  "
+    when 10
+      number.to_s
+    else
+      number.to_s + " "
   end
 end
 
 def board_visualization
-  board.each
+  board.each do |row|
+    p row
+  end
+end
 
 def both_board_visualations
   over_all_game_board.each do |board|
@@ -53,20 +58,11 @@ def over_all_game_board
   both_boards_array = board * 2
 end
 
-#p board_columns
-#
-#board
-board_visualations[0]
-board_visualations[1]
+board_visualization
 
 
 
 
 
 
-
-
-#DRIVER CODE
-
-# board_size
 
